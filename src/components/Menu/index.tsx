4224311234b08167d9fedc5f49ed7803e422ec6d
JayJay <81240900+hijayjay@users.noreply.github.com>
2021-04-12 12:50:19 +0700

    Update index.tsx

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -108,9 +108,9 @@ export default function Menu() {
             <Code size={14} />
             Code
           </MenuItem>
-          <MenuItem id="link" href="https://mallsbase.com">
+          <MenuItem id="link" href="https://t.me/SafeDAO">
             <MessageCircle size={14} />
-            <TranslatedText translationId={34}>Our Partner</TranslatedText>
+            <TranslatedText translationId={34}>Telegram</TranslatedText>
           </MenuItem>
           <MenuItem id="link" href="https://pancakeswap.info">
             <PieChart size={14} />
